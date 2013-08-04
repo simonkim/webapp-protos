f2df347297484eea5fe4e27a367a363e6467d5dc
Simon Kim <spamhere@kaoma.net>
2013-08-04 16:56:58 +0900

    slideshow: blueimp Gallery Test with Fullscreen

--- public/slideshow/js/app_onload.js
+++ public/slideshow/js/app_onload.js
@@ -1,36 +1,17 @@
 /* AJAX Demonstration: load tagline.txt and display under the product name. div tag id=tagline */
 
 
-function create_xmlhttp() {
-    var xmlhttp;
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    return xmlhttp;
-}
-
 function app_load_template( elementId, url, done ) {
     console.log( 'app_load_template( id:' + elementId + ', url:' + url + ' )' );
-
-    /* Argument Verification */
-    if ( !document.getElementById(elementId) ) {
-        console.log( ' - id not found:' + elementId );
-        return;
-    }
-    var xmlhttp = create_xmlhttp();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById(elementId).innerHTML = xmlhttp.responseText;
+    $.ajax({
+        url: url,
+        dataType: 'html',
+        success: function(data) {
+            $('#' + elementId).html(data);
             console.log( 'DONE: app_load_template( id:' + elementId + ', url:' + url + ' )' );
             if ( done ) done();
         }
-    } 
-    xmlhttp.open("GET", url ,true); 
-    xmlhttp.send();
+    });
 }
 
 /*
@@ -44,5 +25,5 @@ app_load_template( "template_header", "templates/header.html", function() {
 });
 app_load_template( "template_footer", "templates/footer.html" );
 app_load_template( "template_body", "templates/body.html", function() {
-   sshow_api_init();
-} );
+
+});
